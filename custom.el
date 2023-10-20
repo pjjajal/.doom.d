fabd53d3efe92c64db8491844111158810bbf8e9
pjjajal <jajlpurvish@gmail.com>
2023-10-20 13:55:30 -0400

    first commit

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("/Users/jajal/gnosis/functional-analysis.org" "/Users/jajal/gnosis/information-theory-inference-and-learning.org" "/Users/jajal/gnosis/paper-readings.org" "/Users/jajal/gnosis/pytorch-notes.org" "/Users/jajal/gnosis/journal/2023-10-20"))
+ '(org-journal-date-format "%A, %d %B %Y")
+ '(org-journal-date-prefix "* ")
+ '(org-journal-dir "/Users/jajal/gnosis/journal/")
+ '(org-journal-enable-agenda-integration t)
+ '(org-journal-file-format "%Y-%m-%d")
+ '(org-journal-file-header "#+TITLE: %A, %d %B %Y")
+ '(org-journal-find-file 'find-file)
+ '(org-journal-time-format "")
+ '(org-journal-time-prefix "** "))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
